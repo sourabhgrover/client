5b45c7f2c7469e3d7ce5617bfe084f3c487816b3
sourabh grover <sourabh.grover@trantorindia.com>
2019-05-30 13:34:54 +0530

    Router Setup and Scaffold components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+import StreamList from "./streams/StreamList";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui conatiner">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
