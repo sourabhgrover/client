a8e31f5d1fa074e14908c9ef513dce877a4ecace
sourabh grover <sourabh.grover@trantorindia.com>
2019-06-02 18:25:15 +0530

    List Stream

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -25,9 +27,11 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = formValues => {
+    // console.log(formValues);
+    this.props.createStream(formValues);
+  };
+
   render() {
     return (
       <form
@@ -60,4 +64,8 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({ form: "streamCreate", validate })(StreamCreate);
+const formWrapper = reduxForm({ form: "streamCreate", validate })(StreamCreate);
+export default connect(
+  null,
+  { createStream }
+)(formWrapper);
