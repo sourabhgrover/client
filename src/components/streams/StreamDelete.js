2e7adfe615d8b5c8edef2baf8952e6f8ae7e343f
sourabh grover <sourabh.grover@trantorindia.com>
2019-06-04 15:06:30 +0530

    Delete STream

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,26 +1,57 @@
 import React from "react";
 
 import Modal from "../Modal";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchStream, deleteStream } from "../../actions";
 import history from "../../history";
 
-const StreamDelete = () => {
-  const action = (
-    <React.Fragment>
-      <button className="ui primary button negative">Delete</button>
-      <button className="ui primary button">Cancel</button>
-    </React.Fragment>
-  );
-  return (
-    <div>
-      StreamDelete
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  dismissAction = () => {
+    return (
+      <React.Fragment>
+        <button
+          onClick={() => this.props.deleteStream(this.props.match.params.id)}
+          className="ui primary button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui primary button">
+          Cancel
+        </Link>
+      </React.Fragment>
+    );
+  };
+
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete the stream";
+    }
+    return `Are you sure you want to delete the stream with title: ${
+      this.props.stream.title
+    }`;
+  }
+
+  render() {
+    return (
       <Modal
         title="Delete Stream"
-        content="Delete Stream"
-        action={action}
+        content={this.renderContent()}
+        action={this.dismissAction()}
         onDismiss={() => history.push("/")}
       />
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.stream[ownProps.match.params.id] };
 };
 
-export default StreamDelete;
+export default connect(
+  mapStateToProps,
+  { fetchStream, deleteStream }
+)(StreamDelete);
