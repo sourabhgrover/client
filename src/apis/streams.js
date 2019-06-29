0080a78d1d0e9bfe70ca644ada45abd249526a45
sourabh <sourabh.grover@trantorinc.com>
2019-06-29 20:58:51 +0530

    Updated JSON Server API path

--- src/apis/streams.js
+++ src/apis/streams.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001"
+  // baseURL: "http://localhost:3001"
+  baseURL: "https://my-json-server.typicode.com/sourabhgrover/stream-api"
 });
